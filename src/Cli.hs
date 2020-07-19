2a9d431a01ae8753699ead9848f207fe4028cbc3
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2020-07-19 15:31:49 +0900

    init new

--- src/Cli.hs
+++ src/Cli.hs
@@ -4,10 +4,12 @@ import qualified AtCoder
 import           Control.Monad.Except           (runExceptT)
 import           Options.Applicative
 import qualified Cli.Login as Login
+import qualified Cli.New as New
 
 data Command
   = Hello String
   | Login
+  | New String
   deriving (Show)
 
 parseHello :: Parser Command
@@ -16,17 +18,24 @@ parseHello = Hello <$> argument str (metavar "[NAME]")
 parseLogin :: Parser Command
 parseLogin = pure Login
 
+parseNew :: Parser Command
+parseNew = New <$> argument str (metavar "[CONTEST_NAME]")
+
 withInfo :: Parser a -> String -> ParserInfo a
 withInfo opts desc = info (helper <*> opts) $ progDesc desc
 
 parseCommand :: Parser Command
 parseCommand =
   subparser $
-    command "hello" (parseHello `withInfo` "Say Hello")
-      <> command "login" (parseLogin `withInfo` "Login AtCoder")
+    command "hello" (parseHello `withInfo` "Say Hello.")
+      <> command "login" (parseLogin `withInfo` "Login AtCoder.")
+      <> command "new" (parseNew `withInfo` "Create a new project.")
 
 parseInfo :: ParserInfo Command
-parseInfo = parseCommand `withInfo` "--help"
+parseInfo = parseCommand `withInfo` usage
+
+usage :: String
+usage = "stack atcoder [--help] [COMMAND]"
 
 execCommand :: IO ()
 execCommand = execParser parseInfo >>= run
@@ -34,3 +43,4 @@ execCommand = execParser parseInfo >>= run
     run cmd = case cmd of
       Hello n -> print $ "Hello, " <> n
       Login   -> Login.login
+      New name -> New.new name
---
+++ src/Cli/New.hs
@@ -0,0 +1,4 @@
+module Cli.New where
+
+new :: String -> IO ()
+new contest = putStrLn contest
